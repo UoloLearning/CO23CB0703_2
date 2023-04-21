a2b05e4a2a46c538bfbe8b39ee4243addc90940d
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-21 17:34:26 +0530

    Update CO23CB0703_2_A1.py

--- CO23CB0703_2_A1.py
+++ CO23CB0703_2_A1.py
@@ -3,3 +3,6 @@ def student(name, age, grade="N/A", school="N/A"):
     print("Age:", age)
     print("Grade:", grade)
     print("School:", school)
+
+student("John Smith", 15, "9th", "XYZ High School")
+student("Jane Doe", 16)

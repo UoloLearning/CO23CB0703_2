c52e8bb94d3de39a347cb45e0ce3d5c52370c16a
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-21 18:25:27 +0530

    Update CO23CB0703_2_AA1.py

--- CO23CB0703_2_AA1.py
+++ CO23CB0703_2_AA1.py
@@ -1,1 +1,9 @@
-
+def check_attendance(roll_number):
+    # For the purpose of this example, let's just return "Present" for even roll numbers and "Absent" for odd roll numbers.
+    if roll_number % 2 == 0:
+        print(f"Attendance status for roll number {roll_number}:","Present")
+    else:
+        print(f"Attendance status for roll number {roll_number}:","Absent")
+        
+roll_number = int(input("Enter the roll number : "))
+check_attendance(roll_number)
